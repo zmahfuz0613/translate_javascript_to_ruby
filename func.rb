c5de1aa27a7b2ef35e17872f341969d795f2c2b9
Zunayed Mahfuz <zunayed.eshna@gmail.com>
2020-09-03 01:25:07 -0400

    Reverse It done

--- func.rb
+++ func.rb
@@ -1,5 +1,28 @@
+# Get Name
+
 def getName()
   name =  puts "what is your name?"
   return name
   end
   getName
+
+  # Reverse It
+
+  string = "a man, a plan, a canal, frenemies!"
+
+  reverse = string.reverse
+  puts "#{reverse}"
+
+  # Another way for Reverse It
+#   def reverseIt()
+#   string = "a man, a plan, a canal, frenemies!"
+
+#   reverse = ""
+#   for i in l..string.length
+#     reverse+=string[string.length - i]
+#   end
+
+#   puts "{reverse}"
+# end
+
+# Multiply Array
